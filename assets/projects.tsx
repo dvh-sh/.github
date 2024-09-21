46753b8dd31349b82120b4983691d0144012d1a0
David <175678329+dvhsh@users.noreply.github.com>
2024-09-20 19:51:34 -0700

    Create projects.tsx

---
+++ assets/projects.tsx
@@ -0,0 +1,10 @@
+{
+  "projects": [
+    {
+      "title": "Project 1",
+      "description": "Description of project 1",
+      "technologies": ["React", "Node.js"],
+      "link": "#"
+    }
+  ]
+}
